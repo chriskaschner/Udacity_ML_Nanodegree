036aead2de5320ad8076325579d0b3f9a49a6ec0
Chris Kaschner <chris.kaschner@gmail.com>
2016-08-22 13:53:45 -0500

    Add basic auth to REST API

--- Professional Development/flask/app.py
+++ Professional Development/flask/app.py
@@ -1,17 +1,30 @@
 import numpy as np
 import tensorflow as tf
-import argparse
+# import argparse
 import json
-import requests
-from pprint import pprint
-import urllib, cStringIO
-from PIL import Image
-# from urllib import urlretrieve
-from flask import Flask, jsonify, abort, make_response, request
+# import requests
 
+import urllib
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask.ext.httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 
+@auth.get_password
+def get_password(username):
+    if username == 'ReturnPath':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized Access'}), 401)
+
 # datastore for this example; ###todo - add database
+###todo replace URL with URI to be overly pedantic
+###todo sort out img vs image vs images usage
+
 images = [
     {
         'id': 1,
@@ -24,15 +37,28 @@ images = [
         'url': 'http://imgdirect.s3-website-us-west-2.amazonaws.com/altra.jpg'
     }
 ]
+
+def make_public_img(image):
+    new_image={}
+    for field in image:
+        if field == 'id':
+            new_image['uri']= url_for('get_image', img_id=image['id'], _external=True)
+        else:
+            new_image[field] = image[field]
+    return new_image
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/img/api/v1.0/images', methods=['GET'])
 def get_images():
-    return jsonify({'images': images})
+    return jsonify({'images': [make_public_img(image) for image in images]})
 
+### test String
+### curl -u ReturnPath:python -i http://127.0.0.1:5000/img/api/v1.0/images/2
 @app.route('/img/api/v1.0/images/<int:img_id>', methods=['GET'])
+@auth.login_required
 def get_image(img_id):
     img = [img for img in images if img['id'] == img_id]
     if len(img) == 0:
@@ -40,9 +66,10 @@ def get_image(img_id):
     return jsonify({'img': img[0]})
 
 ### test String
-### curl -i -H "Content-Type: application/json" -X POST -d '{"url":"http://imgdirect.s3-website-us-west-2.amazonaws.com/neither.jpg"}' http://127.0.0.1:5000/img/api/v1.0/images
+### curl  -u ReturnPath:python -i -H "Content-Type: application/json" -X POST -d '{"url":"http://imgdirect.s3-website-us-west-2.amazonaws.com/neither.jpg"}' http://127.0.0.1:5000/img/api/v1.0/images
 @app.route('/img/api/v1.0/images', methods=['POST'])
-def create_task():
+@auth.login_required
+def create_image():
     if not request.json or not 'url' in request.json:
         abort(400)
 
@@ -55,26 +82,45 @@ def create_task():
         'url': request.json['url'],
         ###todo add logic to retrieve results from image prediction
         'results': run_inference_on_image(request.json['url'])
-
     }
     images.append(image)
-    return jsonify({'image': image}), 201
+    return jsonify({'image': make_public_img(image)}), 201
+
+### test String
+### curl  -u ReturnPath:python -i -H "Content-Type: application/json" -X PUT -d '{"title":"C-ron-ron"}' http://127.0.0.1:5000/img/api/v1.0/images/3
+@app.route('/img/api/v1.0/images/<int:img_id>', methods=['PUT'])
+@auth.login_required
+def update_image(img_id):
+    img = [img for img in images if img['id'] == img_id]
+    if len(img) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    img[0]['title'] = request.json.get('title', img[0]['title'])
+    img[0]['url'] = request.json.get('url', img[0]['url'])
+    return jsonify({'img': img[0]})
+
+### test String
+### curl  -u ReturnPath:python -i -H "Content-Type: application/json" -X DELETE -d http://127.0.0.1:5000/img/api/v1.0/images/3
+@app.route('/img/api/v1.0/images/<int:img_id>', methods=['DELETE'])
+@auth.login_required
+def delete_image(img_id):
+    img = [img for img in images if img['id'] == img_id]
+    if len(img) == 0:
+        abort(404)
+    images.remove(img[0])
+    return jsonify({'result': True})
 
 
-# imagePath = './Report/images/AltraDifficult01.jpg'
-# imgURL = 'http://imgdirect.s3-website-us-west-2.amazonaws.com/nike.jpg'
+### Model and Labels files for TensorFlow
 modelFullPath = './output_graph.pb'
 labelsFullPath = './output_labels.txt'
+
 results_name = []
 results_score = []
 results = []
-
-# Download the file from `url`, save it in a temporary directory and get the
-# path to it (e.g. '/tmp/tmpb48zma.txt') in the `file_name` variable:
-# imagePath, headers = urllib.urlretrieve(imgURL)
-#
-# imgFile = cStringIO.StringIO(urllib.urlopen(imgURL).read())
-# img = Image.open(imgFile)
 
 def create_graph():
     """Creates a graph from saved GraphDef file and returns a saver."""
@@ -125,66 +171,5 @@ def run_inference_on_image(imgURL):
         }
         return results_dict
 
-## should use "try... "
-# def is_valid_file(parser, arg):
-#     try:
-#
-#     if not os.path.exists(arg):
-#         parser.error("The file %s does not exist!" % arg)
-#     else:
-#         return open(arg, 'r')  # return an open file handle
-
-# if __name__ == '__main__':
-#     run_inference_on_image()
-
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="analyzes image for brand logos"
-    )
-    parser.add_argument(
-        "-i", "--image_url",
-        help="The image URL to process",
-        required=True
-    )
-    parser.add_argument(
-        "-e", "--endpoint",
-        help="The API Gateway endpoint to use",
-        required=False,
-        default='https://596kyescv8.execute-api.us-east-1.amazonaws.com/dev/detect_image'
-    )
-# # http://stackoverflow.com/questions/11540854/file-as-command-line-argument-for-argparse-error-message-if-argument-is-not-va
-# # path to graph file
-#         "-g", "--graph",
-#         help="path to graph file to use in TensorFlow, Default: %(default)s",
-#         default='./output_graph.pb',
-#         type=int
-#     )
-
-    args = parser.parse_args()
-
-    # imagePath = args.image_url
-    # print "image path is-",imagePath
-    imgURL = args.image_url
-    imagePath, headers = urllib.urlretrieve(imgURL)
-    # run_inference_on_image()
-
-    # post_params = {
-        # "image_url": args.image_url,
-        # "results_name_1": results_name[0],
-        # "results_score_1": json.JSONEncoder().encode(format(results_score[0], '.5f')),
-        # "results_name_2": results_name[1],
-        # "results_score_2": json.JSONEncoder().encode(format(results_score[1], '.5f')),
-        # "results_name_3": results_name[2],
-        # "results_score_3": json.JSONEncoder().encode(format(results_score[2], '.5f'))
-    # }
-    #
-    # # Lazy and used requests in addition to urllib2
-    # print "post params are- ", post_params
-    # print "results are: ", results
-    # r = requests.post(args.endpoint,
-    #                   data=json.dumps(post_params),
-    #                   headers={'content-type': 'application/json'})
-    # detection_results = r.json()
-    # pprint(detection_results)
     app.run(debug=True)
